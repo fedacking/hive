c41a7ff23b106faaf481f9b8221e0f1878b19879
Francisco Xavier Gauna <francisco.gauna@lambdaclass.com>
2025-05-08 09:13:58 -0300

    Error out if pre merge fork requested

--- clients/ethrex/ethrex.sh
+++ clients/ethrex/ethrex.sh
@@ -97,10 +97,13 @@ fi
 # Configure RPC.
 FLAGS="$FLAGS --http.addr=0.0.0.0  --authrpc.addr=0.0.0.0"
 
+# We don't support pre merge so we exit with an error if HIVE_TERMINAL_TOTAL_DIFFICULTY is set
 if [ "$HIVE_TERMINAL_TOTAL_DIFFICULTY" != "" ]; then
     JWT_SECRET="7365637265747365637265747365637265747365637265747365637265747365"
     echo -n $JWT_SECRET > /jwt.secret
     FLAGS="$FLAGS  --authrpc.jwtsecret=/jwt.secret"
+else
+    exit 1
 fi
 
 FLAGS="$FLAGS  $HIVE_ETHREX_FLAGS"
